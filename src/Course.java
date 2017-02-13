b5cdfeb7a7d71d69606595677eacbacf1231246f
wchen385 <wchen385@uwo.ca>
2017-02-13 17:04:27 -0500

    Added Course class and made changes to Student and Instructor

---
+++ src/Course.java
@@ -0,0 +1,12 @@
+/**
+ * Created by ryanchen1 on 2017-02-13.
+ */
+public class Course {
+    String cname;
+    String description;
+
+    public Course(String cname, String description) {
+           this.cname=cname;
+           this.description=description;
+    }
+}
--- src/Student.java
+++ src/Student.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
 /**
  * Created by ethan on 2017-02-07.
  */
 public class Student extends Person {
     String major;
+    List<Course> courseList;
 
     public Student(String name, String emailAddress, String major) {
         super(name, emailAddress);
@@ -12,4 +15,12 @@ public class Student extends Person {
     public String toString() {
         return String.format("Name: %s\nMajor: %s\nEmail: %s", this.name, this.major, this.emailAddress);
     }
+     public void addCourse(Course c){
+        if(this.courseList==null)
+            this.courseList=new ArrayList<>();
+        this.courseList.add(c);
+     }
+
+
+
 }
